4eb01ad7a8db6e7ebdcdb3a519d75345ae320e68
Hamidreza <Kiani.hamidreza@gmail.com>
2019-08-27 08:08:11 +0430

    add empty solution and add data layer project and win client project to it

---
+++ SalaryApp/SalaryApp.DataLayer/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryApp.DataLayer.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ SalaryApp/SalaryApp.DataLayer/Models/Workshop.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryApp.DataLayer.Models
+{
+    public class Workshop
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } 
+        public string Address { get; set; }
+        public string Tel { get; set; }
+    }
+}
