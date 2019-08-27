48c8f37c468a27a04d9ac330faf5b000d740e671
Hamidreza <Kiani.hamidreza@gmail.com>
2019-08-27 08:15:04 +0430

    add employee model

--- SalaryApp/SalaryApp.DataLayer/Models/Employee.cs
+++ SalaryApp/SalaryApp.DataLayer/Models/Employee.cs
@@ -8,5 +8,17 @@ namespace SalaryApp.DataLayer.Models
 {
     public class Employee
     {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string FatherName { get; set; }
+        public string IdNumber { get; set; }
+        public string NationalCode { get; set; }
+        public int Age { get; set; }
+        public bool IsMarrid { get; set; }
+        public byte NumberOfChildren { get; set; }
+        public string Address { get; set; }
+        public string Mobile { get; set; }
+
     }
 }
