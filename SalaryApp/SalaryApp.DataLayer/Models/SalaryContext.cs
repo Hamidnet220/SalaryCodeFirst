e6bf124b3a724fcf7bf979a16aced4896ec15701
Hamidreza <Kiani.hamidreza@gmail.com>
2019-08-27 17:06:52 +0430

    add salaryContext class as DbContext

---
+++ SalaryApp/SalaryApp.DataLayer/Models/SalaryContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryApp.DataLayer.Models
+{
+    public class SalaryContext:DbContext
+    {
+        public DbSet<Employee> Employee { get; set; }
+
+        public DbSet<Workshop> Workshop { get;set; }
+
+        public SalaryContext():base("name=SalaryContext")
+        {
+              
+        }
+    }
+}
