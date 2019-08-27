0a6c50ed0ab4ae5db660b49a745eb7b29e0bff14
Hamidreza <Kiani.hamidreza@gmail.com>
2019-08-27 08:18:28 +0430

    Add workplace model

---
+++ SalaryApp/SalaryApp.DataLayer/Models/WorkPlace.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryApp.DataLayer.Models
+{
+    public class WorkPlace
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Address { get; set; }
+    }
+}
