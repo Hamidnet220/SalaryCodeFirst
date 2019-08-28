beec721656ad065d32ca8b07fe38a045d1fdf2de
Hamidreza <Kiani.hamidreza@gmail.com>
2019-08-28 06:45:49 +0430

    add Workshop repository and interface

---
+++ SalaryApp/SalaryApp.DataLayer/Models/IWorkshopRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryApp.DataLayer.Models
+{
+    public interface IWorkshopRepository:IRepository<Workshop>
+    {
+        IEnumerable<Workshop> GetByTitle(string value);
+    }
+}
---
+++ SalaryApp/SalaryApp.DataLayer/Models/WorkshopRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryApp.DataLayer.Models
+{
+    public class WorkshopRepository : Repository<Workshop>,IWorkshopRepository
+    {
+        public WorkshopRepository(DbContext context) : base(context)
+        {
+        }
+
+        public SalaryContext SalaryContext
+        {
+            get { return context as SalaryContext; }
+        }
+
+        public IEnumerable<Workshop> GetByTitle(string value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
