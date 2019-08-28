478924b1aca31cac211776efe493433e2f53fb28
Hamidreza <Kiani.hamidreza@gmail.com>
2019-08-28 06:09:19 +0430

    add migration of insert WorkPlace model

---
+++ SalaryApp/SalaryApp.DataLayer/Migrations/201908280133026_add Workplace model.cs
@@ -0,0 +1,28 @@
+namespace SalaryApp.DataLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addWorkplacemodel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Workshops",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Address = c.String(),
+                        Tel = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Workshops");
+        }
+    }
+}
