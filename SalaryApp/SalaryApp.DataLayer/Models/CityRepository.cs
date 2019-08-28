8d30a5bdcf5cda6714528d0c144d04164fce7108
Hamidreza <Kiani.hamidreza@gmail.com>
2019-08-28 09:05:20 +0430

    add city model repo and interface

---
+++ SalaryApp/SalaryApp.DataLayer/Models/CityRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryApp.DataLayer.Models
+{
+    public class CityRepository : Repository<City>, ICityRepository
+    {
+        public CityRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
