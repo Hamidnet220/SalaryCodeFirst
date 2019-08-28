d3b1f99c791ad298a75c18224dec74805428e338
Hamidreza <Kiani.hamidreza@gmail.com>
2019-08-28 05:56:54 +0430

    Implement employee interface

--- SalaryApp/SalaryApp.DataLayer/Models/EmployeeRepository.cs
+++ SalaryApp/SalaryApp.DataLayer/Models/EmployeeRepository.cs
@@ -14,12 +14,17 @@ namespace SalaryApp.DataLayer.Models
         }
         public IEnumerable<Employee> GetByAge(int value)
         {
-            return SalaryContext
+            return SalaryContext.Employee.Where(emp => emp.Age == value);
         }
 
         public IEnumerable<Employee> GetByNationalCode(string NCode)
         {
             throw new NotImplementedException();
+        }
+
+        public SalaryContext SalaryContext
+        {
+            get { return context as SalaryContext; }
         }
     }
 }
