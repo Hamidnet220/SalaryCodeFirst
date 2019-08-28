be7c3667dc6774d279b82d6a30c62d1d881d60b1
Hamidreza <Kiani.hamidreza@gmail.com>
2019-08-28 05:59:56 +0430

    add Unit of work interface and implementation

---
+++ SalaryApp/SalaryApp.DataLayer/Models/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryApp.DataLayer.Models
+{
+    public interface IUnitOfWork:IDisposable
+    {
+        IEmployeeRepository Employees { get; }
+        int Complete();
+    }
+}
---
+++ SalaryApp/SalaryApp.DataLayer/Models/UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryApp.DataLayer.Models
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly SalaryContext context;
+
+        public UnitOfWork(SalaryContext context)
+        {
+            this.context = context;
+            Employees = new EmployeeRepository(context);
+        }
+
+        public IEmployeeRepository Employees { get; private set; }
+
+       
+
+        public int Complete()
+        {
+            return context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
