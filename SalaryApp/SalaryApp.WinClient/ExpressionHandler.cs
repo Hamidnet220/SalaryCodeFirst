0409f853668fa92accac81e372f1f943c3e32ec7
Hamidreza <Kiani.hamidreza@gmail.com>
2019-08-30 10:42:38 +0430

    Add ExpressionHandler class in winclient project for get property name and visit expression.

---
+++ SalaryApp/SalaryApp.WinClient/ExpressionHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace SalaryApp.WinClient
+{
+    public class ExpressionHandler: System.Linq.Expressions.ExpressionVisitor
+    {
+        List<string> visitedProperties = new List<string>();
+
+        protected override Expression VisitMember(MemberExpression node)
+        {
+            if (node.Member is PropertyInfo)
+                visitedProperties.Add(node.Member.Name);
+            return base.VisitMember(node);
+        }
+
+        public string GetPropertyName(Expression expression)
+        {
+            Visit(expression);
+            visitedProperties.Reverse();
+            return string.Join(".",visitedProperties);
+        }
+    }
+}
