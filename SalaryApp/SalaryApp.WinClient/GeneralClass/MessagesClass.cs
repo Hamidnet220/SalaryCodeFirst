7bbc62551e153ac509ac9214051cced1b2f75e71
Hamidreza <Kiani.hamidreza@gmail.com>
2019-09-01 07:32:52 +0430

    add GeneralClass Directory.

---
+++ SalaryApp/SalaryApp.WinClient/GeneralClass/MessagesClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryApp.WinClient.GeneralClass
+{
+    public static class MessagesClass
+    {
+        public static string DeleteConfirm
+        {
+            get { return "آیا از حذف رکورد جاری مطمئن هستید؟"; }
+        }
+
+        public static string CriticalCaption
+        {
+            get { return "هشدار!"; }
+        }
+    }
+}
