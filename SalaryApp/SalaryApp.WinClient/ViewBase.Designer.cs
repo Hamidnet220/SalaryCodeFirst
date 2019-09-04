bf0bdc735371652cd96f500815e6143873171dc7
Hamidreza <Kiani.hamidreza@gmail.com>
2019-09-04 12:41:47 +0430

    add a base for view of lists ViewBase.cs

--- SalaryApp/SalaryApp.WinClient/ViewBase.Designer.cs
+++ SalaryApp/SalaryApp.WinClient/ViewBase.Designer.cs
@@ -19,7 +19,7 @@
             }
             base.Dispose(disposing);
         }
-
+     
         #region Windows Form Designer generated code
 
         /// <summary>
@@ -28,22 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.oprationPanel = new System.Windows.Forms.Panel();
+            this.gridPanel = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // oprationPanel
+            // 
+            this.oprationPanel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.oprationPanel.Location = new System.Drawing.Point(711, 0);
+            this.oprationPanel.Name = "oprationPanel";
+            this.oprationPanel.Size = new System.Drawing.Size(133, 452);
+            this.oprationPanel.TabIndex = 0;
+            // 
+            // gridPanel
+            // 
+            this.gridPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridPanel.Location = new System.Drawing.Point(0, 0);
+            this.gridPanel.Name = "gridPanel";
+            this.gridPanel.Size = new System.Drawing.Size(711, 452);
+            this.gridPanel.TabIndex = 1;
             // 
             // ViewBase
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(607, 372);
-            this.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.ClientSize = new System.Drawing.Size(844, 452);
+            this.Controls.Add(this.gridPanel);
+            this.Controls.Add(this.oprationPanel);
             this.Name = "ViewBase";
-            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ViewBase";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        protected System.Windows.Forms.Panel oprationPanel;
+        protected System.Windows.Forms.Panel gridPanel;
     }
 }
--- SalaryApp/SalaryApp.WinClient/ViewBase.cs
+++ SalaryApp/SalaryApp.WinClient/ViewBase.cs
@@ -10,11 +10,36 @@ using System.Windows.Forms;
 
 namespace SalaryApp.WinClient
 {
-    public partial class ViewBase : Form
+    public partial class ViewBase : BaseForm
     {
+        int buttonTop = 0;
+
         public ViewBase()
         {
             InitializeComponent();
+            Load += ViewBase_Load;
+           
+        }
+
+        private void ViewBase_Load(object sender, EventArgs e)
+        {
+            this.Text = "لیست کارکنان";
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        protected void AddAction(string title, Action<Button> onClick)
+        {
+            var btn = new Button();
+            btn.Text = title;
+            btn.Click += (obj, e) =>
+            {
+                onClick(btn);
+            };
+            buttonTop += 27;
+            btn.Top = buttonTop;
+            btn.Left = 45;
+            oprationPanel.Controls.Add(btn);
+
         }
     }
 }
