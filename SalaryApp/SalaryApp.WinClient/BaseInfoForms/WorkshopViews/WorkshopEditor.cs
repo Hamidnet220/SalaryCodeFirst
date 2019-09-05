f696f3ef3515c386b9dfd0292f375d256ccfcca1
Hamidreza <Kiani.hamidreza@gmail.com>
2019-09-05 20:21:33 +0430

    add workshop editor form.

---
+++ SalaryApp/SalaryApp.WinClient/BaseInfoForms/WorkshopViews/WorkshopEditor.cs
@@ -0,0 +1,22 @@
+﻿using SalaryApp.DataLayer.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryApp.WinClient.BaseInfoForms.WorkshopViews
+{
+    public class WorkshopEditor:EditorBase
+    {
+        public WorkshopEditor()
+        {
+            Load += WorkshopEditor_Load;
+        }
+
+        private void WorkshopEditor_Load(object sender, EventArgs e)
+        {
+            AddAllFields<Workshop>();
+        }
+    }
+}
