2b395e3342b16eec0a0a3104da5f7f87f540f6be
Hamidreza <Kiani.hamidreza@gmail.com>
2019-09-05 18:33:21 +0430

    add VerboseAttribute for create persian name filed.

---
+++ SalaryApp/SalaryApp.DataLayer/Core/VerboseNameAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryApp.DataLayer.Core
+{
+    [AttributeUsage(AttributeTargets.Property,AllowMultiple =false,Inherited =false)]
+    public class VerboseNameAttribute:Attribute
+    {
+        public VerboseNameAttribute(string VerboseName)
+        {
+            this.VerboseName = VerboseName;
+        }
+        public string VerboseName { get; set; }
+    }
+}
