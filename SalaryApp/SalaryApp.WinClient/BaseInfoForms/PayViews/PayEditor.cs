2ce8d96405d793dfb2872b68987c4b9dfb25d563
Hamidreza <Kiani.hamidreza@gmail.com>
2019-09-07 10:05:34 +0430

    add logic to Editor and list for Pay views.

--- SalaryApp/SalaryApp.WinClient/BaseInfoForms/PayViews/PayEditor.cs
+++ SalaryApp/SalaryApp.WinClient/BaseInfoForms/PayViews/PayEditor.cs
@@ -4,19 +4,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace SalaryApp.WinClient.BaseInfoForms.PayViews
 {
+
     public class PayEditor:EditorBase
     {
+        Pay payEntity = new Pay();
+
         public PayEditor()
         {
             Load += PayEditor_Load;
+            Accept.Click += Accept_Click;
+        }
+
+        private void Accept_Click(object sender, EventArgs e)
+        {
+            unitOfWork.Pays.Add(payEntity);
         }
 
         private void PayEditor_Load(object sender, EventArgs e)
         {
             AddAllFields<Pay>();
+
+            foreach (var textbox in this.Controls.OfType<TextBox>())
+            {
+                textbox.DataBindings.Add("Text", payEntity, textbox.Name);
+            }
         }
     }
 }
--- SalaryApp/SalaryApp.WinClient/BaseInfoForms/PayViews/PayList.cs
+++ SalaryApp/SalaryApp.WinClient/BaseInfoForms/PayViews/PayList.cs
@@ -1,13 +1,66 @@
-﻿using System;
+﻿using SalaryApp.DataLayer.Core.Domain;
+using SalaryApp.WinClient.CustomeControls;
+using SalaryApp.WinClient.GeneralClass;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace SalaryApp.WinClient.BaseInfoForms.PayViews
 {
     public class PayList: ListBase
     {
+        GridControl<Pay> grid;
 
+        public PayList()
+        {
+            Load += PayList_Load;
+            Load += AddActions;
+            Load += PopulateGrid;
+        }
+
+
+        private void PayList_Load(object sender, EventArgs e)
+        {
+            this.Text = "لیست پرداخت ها";
+            this.WindowState = FormWindowState.Maximized;
+
+        }
+
+        private void PopulateGrid(object sender, EventArgs e)
+        {
+            grid = new GridControl<Pay>(gridPanel);
+
+            grid.AddTextBoxColumn(py => new Pay().MonthId, "شناسه ماه");
+            grid.AddTextBoxColumn(py => new Pay().EmployeesCount, "تعداد کارکنان");
+            grid.AddTextBoxColumn(py => new Pay().Title, "عنوان پرداخت");
+            grid.AddTextBoxColumn(py => new Pay().TotalGrossAmount, "جمع مبلغ ناخالص");
+
+            grid.PopulateDataGridView(unitOfWork.Pays.GetAll());
+        }
+
+        private void AddActions(object sender, EventArgs e)
+        {
+            AddAction("+جدید", button =>
+            {
+
+            });
+
+            AddAction("ویرایش", button =>
+            {
+            });
+
+            AddAction("-حذف", button =>
+            {
+                if (MessageBox.Show(MessagesClass.DeleteConfirm, MessagesClass.CriticalCaption, MessageBoxButtons.YesNo) != DialogResult.Yes)
+                    return;
+
+                unitOfWork.Pays.Remove(grid.GetCurrentItem);
+                unitOfWork.Complete();
+                grid.RemoveCurrentItem();
+            });
+        }
     }
 }
