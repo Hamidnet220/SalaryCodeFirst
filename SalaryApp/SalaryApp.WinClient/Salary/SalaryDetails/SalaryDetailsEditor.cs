a0327376157bf4ab06764ff91ced980b5ad0dccc
Hamidreza <Kiani.hamidreza@gmail.com>
2019-09-07 20:01:47 +0430

    add editor for salary details.

---
+++ SalaryApp/SalaryApp.WinClient/Salary/SalaryDetails/SalaryDetailsEditor.cs
@@ -0,0 +1,22 @@
+﻿using SalaryApp.DataLayer.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryApp.WinClient.Salary.SalaryDetails
+{
+    public class SalaryDetailsEditor:EditorBase
+    {
+        public SalaryDetailsEditor()
+        {
+            Load += SalaryDetailsEditor_Load;
+        }
+
+        private void SalaryDetailsEditor_Load(object sender, EventArgs e)
+        {
+            AddAllFields<SalaryPayDetails>();
+        }
+    }
+}
