2cb0a097f7c83c83f7765f508a4c6d4dd7aeb5d9
Hamidreza <Kiani.hamidreza@gmail.com>
2019-09-07 17:24:49 +0430

    add app status entity.

---
+++ SalaryApp/SalaryApp.DataLayer/Core/Domain/AppStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryApp.DataLayer.Core.Domain
+{
+    public class AppStatus
+    {
+        public int ActiveUserId { get; set; }
+        public int ActiveWorkShopId { get; set; }
+        public int ActiceFinancialYearId { get; set; }
+
+    }
+}
