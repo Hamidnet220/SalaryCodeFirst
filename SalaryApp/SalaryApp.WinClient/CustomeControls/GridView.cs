a3b70ac5aeea6efb91e542ffb6e838a0c90693b8
Hamidreza <Kiani.hamidreza@gmail.com>
2019-09-08 23:48:26 +0430

    add capability of display nested objects.

--- SalaryApp/SalaryApp.WinClient/CustomeControls/GridView.cs
+++ SalaryApp/SalaryApp.WinClient/CustomeControls/GridView.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -18,10 +19,13 @@ namespace SalaryApp.WinClient.CustomeControls
         public GridControl(Control container)
         {
             grid = new DataGridView();
+            grid.CellFormatting += Grid_CellFormatting;
             container.Controls.Add(grid);
 
             SetupDataGridView();
         }
+
+        
 
         private void SetupDataGridView()
         {
@@ -88,6 +92,60 @@ namespace SalaryApp.WinClient.CustomeControls
         public TModel GetCurrentItem
         {
             get{ return bindingSource.Current as TModel; }
+        }
+
+
+        private void Grid_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            if ((grid.Rows[e.RowIndex].DataBoundItem != null) &&
+                (grid.Columns[e.ColumnIndex].DataPropertyName.Contains(".")))
+            {
+                e.Value = BindProperty(
+                grid.Rows[e.RowIndex].DataBoundItem,
+                grid.Columns[e.ColumnIndex].DataPropertyName
+              );
+            }
+        }
+
+        private string BindProperty(object property, string propertyName)
+        {
+
+            string retValue = "";
+
+            if (propertyName.Contains("."))
+            {
+                PropertyInfo[] arrayProperties;
+                string leftPropertyName;
+
+                leftPropertyName = propertyName.Substring(0, propertyName.IndexOf("."));
+                arrayProperties = property.GetType().GetProperties();
+
+                foreach (PropertyInfo propertyInfo in arrayProperties)
+                {
+                    if (propertyInfo.Name == leftPropertyName)
+                    {
+                        retValue = BindProperty(
+                          propertyInfo.GetValue(property, null),
+                          propertyName.Substring(propertyName.IndexOf(".") + 1));
+                        break;
+                    }
+                }
+
+            }
+            else
+            {
+
+                Type propertyType;
+                PropertyInfo propertyInfo;
+
+                propertyType = property.GetType();
+                propertyInfo = propertyType.GetProperty(propertyName);
+                retValue = propertyInfo.GetValue(property, null).ToString();
+
+            }
+
+            return retValue;
+
         }
 
     }
