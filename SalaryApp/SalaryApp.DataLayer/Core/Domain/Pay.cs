b3ab92e1bed396338f6b7904ae4267ec11dcd23c
Hamidreza <Kiani.hamidreza@gmail.com>
2019-09-09 23:53:59 +0430

    add workshop_Id, FinancaialYear_Id, paytype_Id and delete virtual proporty from pay enetity.

--- SalaryApp/SalaryApp.DataLayer/Core/Domain/Pay.cs
+++ SalaryApp/SalaryApp.DataLayer/Core/Domain/Pay.cs
@@ -3,11 +3,12 @@
     public class Pay
     {
         public int Id { get; set; }
+
         [VerboseName("عنوان پرداخت:")]
         public string Title { get; set; }
-        [VerboseName("نام کارگاه:")]
-        public Workshop Workshop { get; set; }
-        public FincancialYear FinancialYear { get; set; }
+        
+        public int Workshop_Id { get; set; }
+        public int FinancialYear_Id { get; set; }
         [VerboseName("شناسه ماه:")]
         public int MonthId { get; set; }
         [VerboseName("تعداد کارکنان:")]
@@ -18,8 +19,7 @@
         public decimal TotalNetAmount { get; set; }
         public byte Status { get; set; }
         public bool IsDeleted { get; set; }
-        [VerboseName("نوع پرداخت")]
-        public virtual PayType PayType { get; set; }
+        public int PayType_Id { get; set; }
 
     }
 
