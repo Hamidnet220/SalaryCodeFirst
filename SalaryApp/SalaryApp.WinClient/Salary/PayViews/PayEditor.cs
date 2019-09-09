977c6e81a17735840f062023f873da87badd5577
Hamidreza <Kiani.hamidreza@gmail.com>
2019-09-10 00:05:27 +0430

    add combobox and binds to entity .

--- SalaryApp/SalaryApp.WinClient/Salary/PayViews/PayEditor.cs
+++ SalaryApp/SalaryApp.WinClient/Salary/PayViews/PayEditor.cs
@@ -17,21 +17,32 @@ namespace SalaryApp.WinClient.BaseInfoForms.PayViews
         {
             Load += PayEditor_Load;
             Accept.Click += Accept_Click;
+            
         }
 
         private void Accept_Click(object sender, EventArgs e)
         {
             unitOfWork.Pays.Add(payEntity);
+            unitOfWork.Complete();
         }
-
         private void PayEditor_Load(object sender, EventArgs e)
         {
-            AddAllFields<Pay>();
+
+            AddTextFields<Pay>();
+            AddComboBox<PayType>(unitOfWork.PayTypes.GetAll().ToList(), "PayTitle", "Id","نوع پرداخت");
+            AddComboBox<Workshop>(unitOfWork.Workshops.GetAll().ToList(), "Title", "Id","کارگاه");
+            AddComboBox<FinancialYear>(unitOfWork.FinancialYears.GetAll().ToList(), "Year", "Id","سال مالی");
 
             foreach (var textbox in this.Controls.OfType<TextBox>())
             {
                 textbox.DataBindings.Add("Text", payEntity, textbox.Name);
             }
+
+            foreach (var comboBox in this.Controls.OfType<ComboBox>())
+            {
+                comboBox.DataBindings.Add("SelectedValue", payEntity,string.Format("{0}_Id",comboBox.Name));
+            }
+
         }
     }
 }
