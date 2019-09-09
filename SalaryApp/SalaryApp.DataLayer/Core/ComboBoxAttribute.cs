c8f1ca153c0d0577f1373eb3153e20e6166707d7
Hamidreza <Kiani.hamidreza@gmail.com>
2019-09-10 00:01:55 +0430

    add combobox attribute.

---
+++ SalaryApp/SalaryApp.DataLayer/Core/ComboBoxAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryApp.DataLayer.Core
+{
+    [AttributeUsage(AttributeTargets.Property,AllowMultiple =false,Inherited =false)]
+    public class ComboBoxAttribute:Attribute
+    {
+        public string SourceName { get; set; }
+
+        public ComboBoxAttribute(string sourceName)
+        {
+            this.SourceName = sourceName;
+        }
+    }
+}
