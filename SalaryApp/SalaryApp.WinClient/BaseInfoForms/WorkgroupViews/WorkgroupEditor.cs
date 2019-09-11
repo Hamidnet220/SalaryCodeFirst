4ca969699e10d5936c7b7226ffd9a73cd22c3333
Hamidreza <Kiani.hamidreza@gmail.com>
2019-09-11 13:36:51 +0430

    add list and editor view for workgropus.

---
+++ SalaryApp/SalaryApp.WinClient/BaseInfoForms/WorkgroupViews/WorkgroupEditor.cs
@@ -0,0 +1,48 @@
+﻿using SalaryApp.DataLayer.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalaryApp.WinClient.BaseInfoForms.WorkgroupViews
+{
+    public class WorkgroupEditor:EditorBase
+    {
+        public Workgroup entity { get; private set; }
+
+        public WorkgroupEditor(Workgroup entity)
+        {
+            this.entity = entity;
+            Load += WorkgroupEditor_Load;
+            Accept.Click += Accept_Click;
+            Cancel.Click += Cancel_Click;
+        }
+
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+        }
+
+        private void Accept_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+        }
+        private void WorkgroupEditor_Load(object sender, EventArgs e)
+        {
+
+            AddTextFields<Workgroup>();
+            AddComboBox(unitOfWork.Workshops.GetAll().ToList(), workshop => workshop.Title, workshop => workshop.Id, "کارگاه", entity, workgroup=>workgroup.Workshop_Id);
+
+            foreach (var textbox in this.Controls.OfType<TextBox>())
+            {
+                textbox.DataBindings.Add("Text", entity, textbox.Name);
+            }
+
+
+
+        }
+    }
+}
---
+++ SalaryApp/SalaryApp.WinClient/BaseInfoForms/WorkgroupViews/WorkgroupList.cs
@@ -0,0 +1,110 @@
+﻿using SalaryApp.DataLayer.Core.Domain;
+using SalaryApp.DataLayer.Persistence;
+using SalaryApp.WinClient.CustomeControls;
+using SalaryApp.WinClient.GeneralClass;
+using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace SalaryApp.WinClient.BaseInfoForms.WorkgroupViews
+{
+    public class WorkgroupList:ListBase
+    {
+        GridControl<Workgroup> grid;
+        Workshop workshop;
+
+        public WorkgroupList(Workshop workshop)
+        {
+            this.workshop = workshop;
+            Load += WorkgroupList_Load;
+            Load += AddActions;
+            Load += PopulateGrid;
+
+        }
+
+        private void WorkgroupList_Load(object sender, EventArgs e)
+        {
+            this.Text = "لیست پرداخت ها";
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void PopulateGrid(object sender, EventArgs e)
+        {
+            grid = new GridControl<Workgroup>(gridPanel);
+
+            grid.AddTextBoxColumn(py => new Workgroup().Title, "عنوان");
+            grid.AddTextBoxColumn(py => new Workgroup().Rate, "پایه روزانه");
+            grid.AddTextBoxColumn(py => new Workgroup().ChildrenBenefit, "مبلغ حق اولاد");
+            grid.AddTextBoxColumn(py => new Workgroup().Bon, "بن");
+            grid.AddTextBoxColumn(py => new Workgroup().Maskan, "مسکن");
+            grid.AddTextBoxColumn(py => new Workgroup().ShiftRation, "درصد شیفت");
+            grid.AddTextBoxColumn(py => new Workgroup().Description, "توضیحات");
+
+            grid.PopulateDataGridView(unitOfWork.Workgroups.Find(wg=>wg.Workshop_Id==this.workshop.Id).ToList());
+        }
+
+        private void AddActions(object sender, EventArgs e)
+        {
+            AddAction("+جدید", button =>
+            {
+                var workgroupEditor = new WorkgroupEditor(new Workgroup());
+                workgroupEditor.ShowDialog();
+
+                if (workgroupEditor.DialogResult == DialogResult.Cancel)
+                    return;
+
+                unitOfWork.Workgroups.Add(workgroupEditor.entity);
+                unitOfWork.Complete();
+                grid.AddItem(workgroupEditor.entity);
+
+            });
+
+            AddAction("ویرایش", button =>
+            {
+                var entity = unitOfWork.Workgroups.Get(grid.GetCurrentItem.Id);
+                var workgroupEditor = new WorkgroupEditor(entity);
+                workgroupEditor.ShowDialog();
+
+                if (workgroupEditor.DialogResult == DialogResult.Cancel)
+                    return;
+
+                unitOfWork.Complete();
+                
+            });
+
+            AddAction("کپی", button =>
+            {
+                var entity = unitOfWork.Workgroups.Get(grid.GetCurrentItem.Id);
+                Type typeSource = entity.GetType();
+                var newEntity = Activator.CreateInstance(typeSource);
+
+                foreach (var property in entity.GetType().GetProperties())
+                {
+                    property.SetValue(newEntity, property.GetValue(entity));
+                }
+
+                var workgroupEditor = new WorkgroupEditor((Workgroup)newEntity);
+                workgroupEditor.ShowDialog();
+
+                if (workgroupEditor.DialogResult == DialogResult.Cancel)
+                    return;
+
+                unitOfWork.Workgroups.Add(workgroupEditor.entity);
+                unitOfWork.Complete();
+                grid.AddItem(workgroupEditor.entity);
+            });
+
+
+            AddAction("-حذف", button =>
+            {
+                if (MessageBox.Show(MessagesClass.DeleteConfirm, MessagesClass.CriticalCaption, MessageBoxButtons.YesNo) != DialogResult.Yes)
+                    return;
+
+                unitOfWork.Workgroups.Remove(grid.GetCurrentItem);
+                unitOfWork.Complete();
+                grid.RemoveCurrentItem();
+            });
+        }
+    }
+
+}
