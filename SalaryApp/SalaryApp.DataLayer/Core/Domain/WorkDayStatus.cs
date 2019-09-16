d99f2ae225af5bae445ba6c35eea154489134bfe
Hamidreza <Kiani.hamidreza@gmail.com>
2019-09-16 07:51:16 +0430

    add WorkdayStatus entity.

---
+++ SalaryApp/SalaryApp.DataLayer/Core/Domain/WorkDayStatus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryApp.DataLayer.Core.Domain
+{
+    public class WorkDayStatus
+    {
+        public int Id { get; set; }
+
+        public int Code { get; set; }
+
+        public string Ttile { get; set; }
+
+        public string ShortName { get; set; }
+
+        public bool IsWorkDay { get; set; }
+    }
+}
