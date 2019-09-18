96f921cce585810937857d56eb509ccd56652948
Hamidreza <Kiani.hamidreza@gmail.com>
2019-09-18 06:59:57 +0430

    Add LogsheetRpo and LogsheetIRepo to DataLayer.

---
+++ SalaryApp/SalaryApp.DataLayer/Core/Repositories/ILogsheetRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SalaryApp.DataLayer.Core.Domain;
+
+namespace SalaryApp.DataLayer.Core.Repositories
+{
+    public interface ILogsheetRepository:IRepository<Logsheet>
+    {
+
+    }
+}
---
+++ SalaryApp/SalaryApp.DataLayer/Persistence/Repositories/LogsheetRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+using SalaryApp.DataLayer.Core.Repositories;
+using SalaryApp.DataLayer.Core.Domain;
+
+namespace SalaryApp.DataLayer.Persistence.Repositories
+{
+    public class LogsheetRepository : Repository<Logsheet>, ILogsheetRepository
+    {
+        public LogsheetRepository(DbContext context) : base(context)
+        {
+        }
+
+        public SalaryContext SalaryContext
+        {
+            get { return context as SalaryContext; }
+        }
+    }
+}
