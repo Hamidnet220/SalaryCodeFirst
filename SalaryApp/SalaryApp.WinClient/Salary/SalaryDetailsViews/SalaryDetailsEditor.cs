8dd532b056924fb2b69c973d65da2d117177caa3
Hamidreza <Kiani.hamidreza@gmail.com>
2019-09-18 09:15:52 +0430

    binding textbox to initial editor of salaryDetailsEditor

--- SalaryApp/SalaryApp.WinClient/Salary/SalaryDetailsViews/SalaryDetailsEditor.cs
+++ SalaryApp/SalaryApp.WinClient/Salary/SalaryDetailsViews/SalaryDetailsEditor.cs
@@ -4,19 +4,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace SalaryApp.WinClient.Salary.SalaryDetails
 {
     public class SalaryDetailsEditor:EditorBase
     {
-        public SalaryDetailsEditor()
+        public SalaryPayDetails entity { get; private set; }
+
+        public SalaryDetailsEditor(SalaryPayDetails entity)
         {
+            this.entity = entity;
             Load += SalaryDetailsEditor_Load;
         }
 
         private void SalaryDetailsEditor_Load(object sender, EventArgs e)
         {
             AddTextFields<SalaryPayDetails>();
+
+            foreach (var textbox in this.Controls.OfType<TextBox>())
+            {
+                textbox.DataBindings.Add("Text", entity, textbox.Name);
+            }
         }
+
+
+       
     }
 }
