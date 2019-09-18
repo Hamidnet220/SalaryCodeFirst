14c94eedc56a74224c53251d6d77705e70ce3c5f
Hamidreza <Kiani.hamidreza@gmail.com>
2019-09-18 15:38:20 +0430

    add intial entity for payeditor.

--- SalaryApp/SalaryApp.WinClient/Salary/PayViews/PayEditor.cs
+++ SalaryApp/SalaryApp.WinClient/Salary/PayViews/PayEditor.cs
@@ -1,49 +1,36 @@
-﻿using SalaryApp.DataLayer.Core.Domain;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using SalaryApp.DataLayer.Core.Domain;
 
-namespace SalaryApp.WinClient.BaseInfoForms.PayViews
+namespace SalaryApp.WinClient.Salary.PayViews
 {
 
     public class PayEditor:EditorBase
     {
-        Pay payEntity = new Pay();
+        public Pay entity { get; private set; }
         public event EventHandler<Pay> AddEntity;
 
 
-        public PayEditor()
+        public PayEditor(Pay entity)
         {
+            this.entity = entity;
             Load += PayEditor_Load;
-            Accept.Click += Accept_Click;
             
         }
 
-        private void Accept_Click(object sender, EventArgs e)
-        {
-            unitOfWork.Pays.Add(payEntity);
-            unitOfWork.Complete();
-            if (AddEntity != null)
-                AddEntity(this,payEntity);
-        }
-        private void PayEditor_Load(object sender, EventArgs e)
+       private void PayEditor_Load(object sender, EventArgs e)
         {
 
             AddTextFields<Pay>();
-            AddComboBox(unitOfWork.PayTypes.GetAll().ToList(),paytype=>paytype.PayTitle, paytype => paytype.Id,"نوع پرداخت",payEntity,pay=>new Pay().PayType_Id);
-            AddComboBox(unitOfWork.Workshops.GetAll().ToList(), workshop=>workshop.Title, workshop=>workshop.Id, "کارگاه",payEntity, pay => new Pay().Workshop_Id);
-            AddComboBox(unitOfWork.FinancialYears.GetAll().ToList(), year=>year.Year,year=>year.Id,"سال مالی", payEntity, pay => new Pay().FinancialYear_Id);
+            AddComboBox(unitOfWork.PayTypes.GetAll().ToList(),paytype=>paytype.PayTitle, paytype => paytype.Id,"نوع پرداخت",entity,pay=>new Pay().PayType_Id);
+            AddComboBox(unitOfWork.Workshops.GetAll().ToList(), workshop=>workshop.Title, workshop=>workshop.Id, "کارگاه",entity, pay => new Pay().Workshop_Id);
+            AddComboBox(unitOfWork.FinancialYears.GetAll().ToList(), year=>year.Year,year=>year.Id,"سال مالی", entity, pay => new Pay().FinancialYear_Id);
 
             foreach (var textbox in this.Controls.OfType<TextBox>())
             {
-                textbox.DataBindings.Add("Text", payEntity, textbox.Name);
+                textbox.DataBindings.Add("Text", entity, textbox.Name);
             }
-
-           
 
         }
     }
