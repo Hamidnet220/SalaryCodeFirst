1e5388a449c205f0ef83b0f8e059cc5188152807
Hamidreza <Kiani.hamidreza@gmail.com>
2019-09-18 06:55:46 +0430

    add Logsheet property to UnitOfWork class.

--- SalaryApp/SalaryApp.DataLayer/Persistence/UnitOfWork.cs
+++ SalaryApp/SalaryApp.DataLayer/Persistence/UnitOfWork.cs
@@ -7,39 +7,69 @@ namespace SalaryApp.DataLayer.Persistence
 {
     public class UnitOfWork : IUnitOfWork
     {
+        private readonly SalaryContext context;
+
+
         public IPersonRepository Persons { get; private set; }
+
         public IEmployeeRepository Employees { get; private set; }
+
         public IWorkshopRepository Workshops { get; private set; }
+
         public ICityRepository Cities { get; private set; }
+
         public IWorkgroupRepository Workgroups { get; private set; }
+
         public IFinancialYearRepository FinancialYears { get; private set; }
+
         public IWorkPlaceRepository Workplaces { get; private set; }
+
         public IPayRepository Pays { get; private set; }
+
         public ISalaryPayDetails SalaryDetails { get; private set; }
+
         public IAnnualPayDetailRepository AnnualDetails { get; private set; }
+
         public IEducationRepository Educations { get; private set; }
+
         public IPayTypeRepository PayTypes { get; private set; }
+
         public IUserRepository Users { get; private set; }
 
-        private readonly SalaryContext context;
+        public ILogsheetRepository Logsheets { get; private set; }
+
 
         public UnitOfWork(SalaryContext context)    
         {
             this.context = context;
 
             Persons = new PersonRepository(context);
+
             Employees = new EmployeeRepository(context);
+
             Workshops = new WorkshopRepository(context);
+
             Cities = new CityRepository(context);
+
             Workgroups = new WorkgroupRepository(context);
+
             FinancialYears = new FinancialYearRepository(context);
+
             Workplaces = new WorkplaceRepository(context);
+
             Pays = new PayRepository(context);
+
             SalaryDetails = new SalaryPayDetailsRepository(context);
+
             AnnualDetails = new AnnualPayDetailRepository(context);
+
             Educations = new EducationRepository(context);
+
             PayTypes = new PayTypeRepository(context);
+
             Users = new UserRepository(context);
+
+            Logsheets = new LogsheetRepository(context);
 
         }
 
