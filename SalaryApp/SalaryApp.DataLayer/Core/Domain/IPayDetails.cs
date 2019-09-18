b6d6d14becb2106822ceb877c959733a2bdd6031
Hamidreza <Kiani.hamidreza@gmail.com>
2019-09-18 07:01:03 +0430

    add IPayDetails interface for calculate all type of salary and eche type of salary details implement it.

---
+++ SalaryApp/SalaryApp.DataLayer/Core/Domain/IPayDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryApp.DataLayer.Core.Domain
+{
+    public interface IPayDetails
+    {
+        decimal GetGrossAmount();
+        decimal GetTaxAmount();
+        decimal GetInsuranceAmount();
+        decimal GetNetAmount();
+        
+    }
+}
