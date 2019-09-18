25c0fdb143d5b2eba17f65b90f2923f2984be79d
Hamidreza <Kiani.hamidreza@gmail.com>
2019-09-18 07:01:49 +0430

    add attendanceEngin fo analys and calculate Attendance of employee.

---
+++ SalaryApp/SalaryApp.DataLayer/Core/AttendanceEngin.cs
@@ -0,0 +1,19 @@
+﻿using SalaryApp.DataLayer.Core.Domain;
+
+namespace SalaryApp.DataLayer.Core
+{
+    public class AttendanceEngin
+    {
+        Logsheet logsheet;
+
+        public AttendanceEngin(Logsheet logsheet)
+        {
+            this.logsheet = logsheet;
+        }
+
+        private void AnalyzeAttendance()
+        {
+            
+        }
+    }
+}
