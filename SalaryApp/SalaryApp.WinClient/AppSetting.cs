cd2031472a83eef09e34ecaeb20e8d8093a0b660
Hamidreza <Kiani.hamidreza@gmail.com>
2019-09-21 16:24:38 +0430

    add AppSetting class to change appStatus.

---
+++ SalaryApp/SalaryApp.WinClient/AppSetting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SalaryApp.DataLayer.Core.Domain;
+
+namespace SalaryApp.WinClient
+{
+    public static class AppSetting
+    {
+        public static AppStatus AppStatus { get; set; }
+    }
+}
